d6751df0c45ac98398ea365ad53ac1216285bfc1
Jay Lavine <jaylavine@gmail.com>
2023-02-26 11:23:51 -0800

    cleaned up the files

--- newsrv/variables.tf
+++ newsrv/variables.tf
@@ -10,35 +10,3 @@ variable "interface" {
   type        = string
   description = "Name of the network interface"
 }
-# variable "ctlplanehosts" {
-#   type        = number
-#   description = "Control Plane node hosts quantity"
-# }
-# variable "ctlplanememory" {
-#   type        = string
-#   description = "Control Plane node memory"
-# }
-# variable "ctlplanevcpu" {
-#   type        = number
-#   description = "Control Plane node virtual cpus" 
-# }
-# variable "ctlplaneips" {
-#   type        = list(string)
-#   description = "Control Plane node IP addresses"
-# }
-# variable "ctlplanemacs" {
-#   type        = list(string)
-#   description = "Control Plane node MAC addresses"
-# }
-# variable "ctlplanenames" {
-#   type        = list(string)
-#   description = "Control Plane node names"
-# }
-# variable "ctlplanedisk" {
-#   type        = number
-#   description = "Control Plane node disk size" 
-# }
-# variable "workerdisk" {
-#   type        = number
-#   description = "Worker node disk size" 
-# }
